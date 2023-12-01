5dc7ddbbe6c007f5e0230e5228b1ef5456dea5f9
inma <inma@DESKTOP-8LK2H4M>
2023-12-01 12:00:59 +0100

    metodo devolver entero

--- Prueba-Git-Distancia/src/main/java/modelo/MiClase.java
+++ Prueba-Git-Distancia/src/main/java/modelo/MiClase.java
@@ -6,7 +6,11 @@ public class MiClase implements miInterfaz {
 	
 	public int metododevuelventero() {
 		
-      return 0;		
+		int mientero = 5;
+		int otroentero = mientero-3;
+		int resultado = otroentero;
+		
+      return resultado;		
 	}
 
 }
