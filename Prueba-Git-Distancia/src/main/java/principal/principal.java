371b2f0c9bad0536abbb80e0094e68acf575d55c
inma <inma@DESKTOP-8LK2H4M>
2023-12-01 11:58:44 +0100

    Primera semana de trabajo

--- Prueba-Git-Distancia/src/main/java/principal/principal.java
+++ Prueba-Git-Distancia/src/main/java/principal/principal.java
@@ -4,6 +4,15 @@ public class principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+	System.out.println ("comienzo de mi programa dificilisimo");
+	
+	System.out.println ("Sigo con mi programa dificilisimo");
+	
+	System.out.println ("Añado funcionalidades a mi programa dificilisimo");
+	
+	System.out.println ("Estoy muy contento con mi programa dificilisimo");
+	
 
 	}
 
